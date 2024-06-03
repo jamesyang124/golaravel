7f5b5ab752bb3bf8efa0743e5833daf831d060ee
James Yang <jamesyang124@gmail.com>
2024-06-04 05:29:31 +0800

    ADD. [SEC-9] add application file system mount logic

--- main.go
+++ main.go
@@ -1,12 +1,5 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/jamesyang124/celeritas"
-)
-
 func main() {
-	res := celeritas.TestFunc1(1, 2)
-	fmt.Println(res)
+
 }
--- vendor/github.com/jamesyang124/celeritas/celeritas.go
+++
@@ -1,9 +0,0 @@
-package celeritas
-
-func TestFunc1(a, b int) int {
-	return a + b
-}
-
-func TestFunc2(a, b int) int {
-	return a * b
-}
