56f1228c66b332554963860483f8e4813d8c75ad
James Yang <jamesyang124@gmail.com>
2024-06-13 09:33:52 +0800

    ADD. [SEC-21] add jet template pages and set default renderer to jet

--- routes.go
+++ routes.go
@@ -10,11 +10,6 @@ func (a *application) routes() *chi.Mux {
 
 	a.App.Routes.Get("/", a.Handlers.Home)
 
-	// test jet engine templating
-	a.App.Routes.Get("/jet", func(w http.ResponseWriter, r *http.Request) {
-		a.App.Render.JetPage(w, r, "test", nil, nil)
-	})
-
 	fileServer := http.FileServer(http.Dir("./public"))
 	// http.StripPrefix handler will strip HTTP request's url prefix then send it to correspoding handler
 	a.App.Routes.Handle("/public/*", http.StripPrefix("/public", fileServer))
