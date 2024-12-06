03de490785dc357952234c8621eb2f68b48761e4
James Yang <jamesyang124@gmail.com>
2024-12-06 23:02:47 +0800

    UPD. [SEC-36] add db query handling and its test route

--- routes.go
+++ routes.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -12,6 +13,21 @@ func (a *application) routes() *chi.Mux {
 	a.App.Routes.Get("/go-page", a.Handlers.GoTemplatePage)
 	a.App.Routes.Get("/jet-page", a.Handlers.JetTemplatePage)
 	a.App.Routes.Get("/sessions", a.Handlers.SessionTest)
+
+	a.App.Routes.Get("/test-database", func(w http.ResponseWriter, r *http.Request) {
+		query := "select id, first_name from users where id = 1"
+		row := a.App.DB.Pool.QueryRowContext(r.Context(), query)
+
+		var id int
+		var name string
+		if err := row.Scan(&id, &name); err != nil {
+			a.App.ErrorLog.Println(err)
+			return
+		}
+
+		fmt.Fprintf(w, "%d %s", id, name)
+
+	})
 
 	fileServer := http.FileServer(http.Dir("./public"))
 	// http.StripPrefix handler will strip HTTP request's url prefix then send it to correspoding handler
