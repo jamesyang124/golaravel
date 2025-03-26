c0331cdb9f81d2b9cd9075dfb870dc7069a12e0f
James Yang <jamesyang124@gmail.com>
2025-03-27 01:13:23 +0800

    build: remove vendor folder in repo

--- vendor/github.com/jamesyang124/celeritas/middleware.go
+++
@@ -1,9 +0,0 @@
-package celeritas
-
-import "net/http"
-
-func (c *Celeritas) SessionLoad(next http.Handler) http.Handler {
-	c.InfoLog.Println("SessionLoad called")
-	// load and save session to each handler
-	return c.Session.LoadAndSave(next)
-}
