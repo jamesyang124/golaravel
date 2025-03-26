d10ceedb357fdeca09bc69f114fcc8ab3f70194c
James Yang <jamesyang124@gmail.com>
2025-03-27 01:10:23 +0800

    feat: sec.38 define user/token models and upper db ORM

--- routes.go
+++ routes.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -14,6 +13,7 @@ func (a *application) routes() *chi.Mux {
 	a.App.Routes.Get("/jet-page", a.Handlers.JetTemplatePage)
 	a.App.Routes.Get("/sessions", a.Handlers.SessionTest)
 
+	/** test purpoose only
 	a.App.Routes.Get("/test-database", func(w http.ResponseWriter, r *http.Request) {
 		query := "select id, first_name from users where id = 1"
 		row := a.App.DB.Pool.QueryRowContext(r.Context(), query)
@@ -28,6 +28,7 @@ func (a *application) routes() *chi.Mux {
 		fmt.Fprintf(w, "%d %s", id, name)
 
 	})
+	*/
 
 	fileServer := http.FileServer(http.Dir("./public"))
 	// http.StripPrefix handler will strip HTTP request's url prefix then send it to correspoding handler
